5b8ed8f7634196f926149e7ada204857c7ab9b68
Michi Lehr <mlehr@jimdo.com>
2012-04-19 11:31:34 +0200

    adds cache to board detail

--- src/Jimdo/JimKanWall/ViewBoardBundle/Controller/BoardController.php
+++ src/Jimdo/JimKanWall/ViewBoardBundle/Controller/BoardController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Jimdo\JimKanWall\ImportBundle\Entity\Board;
+use \Symfony\Component\HttpFoundation\Response;
 
 class BoardController extends Controller
 {
@@ -31,13 +32,29 @@ class BoardController extends Controller
     public function detailAction($boardId)
     {
         $em = $this->get('doctrine')->getEntityManager();
+
+        $response = new Response();
+        $response->setPublic();
+
+        $newestSnapShot = $em->getRepository('Jimdo\JimKanWall\ImportBundle\Entity\SnapShot')->getNewestSnapShotByBoardId($boardId);
+
+        $response->setETag(md5($newestSnapShot->getId() . $newestSnapShot->getCreatedAt()->format('Y-m-d H:i:s')));
+
+        if ($response->isNotModified($this->get('request'))) {
+            return $response;
+        }
+
         $boards = $em->getRepository('Jimdo\JimKanWall\ImportBundle\Entity\Board')->findAll();
 
         $entitiToViewMapper = $this->get('jimdo.entity_to_view_mapper');
 
         $snapShot = $entitiToViewMapper->getSnapShotDetail($boardId);
 
-        return array('snapShot' => $snapShot, 'boards' => $boards);
+        return $this->render(
+            'JimdoJimKanWallViewBoardBundle:Board:detail.html.twig',
+            array('snapShot' => $snapShot, 'boards' => $boards),
+            $response
+        );
     }
 
 }
