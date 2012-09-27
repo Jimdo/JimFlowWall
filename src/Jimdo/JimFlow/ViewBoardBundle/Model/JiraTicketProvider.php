2c8faf47a2b4da68554a33f3ffe5b446a3457cb7
michi lehr <michi@jimdo.com>
2012-09-27 15:08:55 +0200

    adds view for jira tickets

--- src/Jimdo/JimFlow/ViewBoardBundle/Model/JiraTicketProvider.php
+++ src/Jimdo/JimFlow/ViewBoardBundle/Model/JiraTicketProvider.php
@@ -9,24 +9,38 @@ class JiraTicketProvider implements TicketProviderInterface {
 
     const CODE_STARTS_WITH = 'J';
     private $ticketFactory;
+    private $jira_protocol;
+    private $jira_url;
+    private $jira_user;
+    private $jira_password;
 
-    public function __construct(TicketFactory $ticketFactory)
+    public function __construct(TicketFactory $ticketFactory, $jira_protocol, $jira_url, $jira_user, $jira_password)
     {
         $this->ticketFactory = $ticketFactory;
+        $this->jira_protocol = $jira_protocol;
+        $this->jira_url = $jira_url;
+        $this->jira_user = $jira_user;
+        $this->jira_password = $jira_password;
 
     }
 
     public function getTicketByCode($code)
     {
-        //todo
         $id = intval(str_replace(self::CODE_STARTS_WITH, '', $code));
 
+        $requestUrl = sprintf ('%s://%s:%s@%s/rest/api/2/issue/%s', $this->jira_protocol, $this->jira_user, $this->jira_password, $this->jira_url, $id);
+        
+        $issueResponse = file_get_contents($requestUrl);
+        
+        $issue = json_decode($issueResponse);
+        
+        $title = $issue->fields->summary;
+        $type =  $issue->fields->issuetype->name;
+        $key = $issue->key;
+        $url = sprintf ('%s://%s/browse/%s', $this->jira_protocol, $this->jira_url, $key);
+        
         $ticket = $this->ticketFactory->build();
 
-        $title = 'My Title';
-        $type = 'My Type';
-        $url = '#';
-        
         $ticket->setTitle($title);
         $ticket->setId($id);
         $ticket->setType($type);
